2d5b4a9f95fbcdb4c4b02b26ed1cc0f07d13dce4
shayake <shayakkhan82@gmail.com>
2026-03-19 02:12:25 +0530

    change the base url

--- src/utils/apiPaths.js
+++ src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ai-power-learner-backend2-1.onrender.com"
+export const BASE_URL = "https://ai-power-learner-backend2-3.onrender.com"
 
 export const API_PATHS = {
     AUTH: {
